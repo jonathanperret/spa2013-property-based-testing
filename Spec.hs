618baef01c704930bd8bc26283ba7bb313481e25
Jonathan Perret <jonathan@ut7.fr>
2013-06-26 10:24:43 +0100

    Different units don't add up
    Fly me away to the bright side of the moon ...

--- Spec.hs
+++ Spec.hs
@@ -7,14 +7,22 @@ import Data.Ratio
 data Unit = Meter | Second
   deriving (Show, Eq)
 
-data Measure = Measure Rational Unit
+data Measure = Measure Rational Unit | InvalidMeasure
   deriving (Show, Eq)
 
+instance Arbitrary Unit where
+  arbitrary = elements [ Meter, Second ]
+
 instance Num Measure where
-   (Measure x Meter) + (Measure y Meter) = (Measure (x + y) Meter)
+  (Measure x u1) + (Measure y u2)
+    | u1 == u2 = (Measure (x + y) u1)
+    | otherwise = InvalidMeasure
 
 main = hspec $ do
   describe "Arithmetic on measures" $ do
     it "adds measures expressed in the same unit" $ property $
-      \(x, y)->
-        (Measure x Meter) + (Measure y Meter) == (Measure (x + y) Meter)
+      \(x, y, u)->
+        (Measure x u) + (Measure y u) == (Measure (x + y) u)
+    it "does adds measures expressed in different units" $ property $
+      \(x, y, u, u2)->
+        u == u2 || (Measure x u) + (Measure y u2) == InvalidMeasure
