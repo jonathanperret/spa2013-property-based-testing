2e0d3edf6e37ff95794b172bf7f0f025ac637da5
Jonathan Perret <jonathan@ut7.fr>
2013-06-26 12:06:43 +0100

    Arbitrary compound units
    Q:	Why did the germ cross the microscope?
    A:	To get to the other slide.

--- Spec.hs
+++ Spec.hs
@@ -4,18 +4,13 @@ import Test.HUnit
 import Test.QuickCheck
 import Data.Ratio
 import Control.Applicative
+import Control.Monad
 
-data Unit = Meter | Second | Unitless | Unit :*: Unit
+data Unit = Meter | Second | Watt | Unitless | Unit :*: Unit
   deriving (Show, Eq)
 
 data Measure = Measure { value :: Rational, unit :: Unit } | InvalidMeasure
   deriving (Show, Eq)
-
-instance Arbitrary Unit where
-  arbitrary = elements [ Meter, Second, Unitless ]
-
-instance Arbitrary Measure where
-  arbitrary = Measure <$> arbitrary <*> arbitrary
 
 instance Num Measure where
   Measure x u1 + Measure y u2
@@ -31,23 +26,42 @@ instance Num Measure where
       combine u1 Unitless = u1
       combine u1 u2 = (u1 :*: u2)
 
+-- Specs start here
+
 main = hspec $ do
   describe "Arithmetic on measures" $ do
     it "adds measures expressed in the same unit" $ property $
       \(x, y, u)->
         Measure x u + Measure y u == Measure (x + y) u
+
     it "does adds measures expressed in different units" $ property $
       \(m1, m2)->
         unit m1 /= unit m2 ==>
           m1 + m2 == InvalidMeasure
+
     it "subtracts measures expressed in the same unit" $ property $
       \(x, y, u)->
         Measure x u - Measure y u == Measure (x - y) u
+
     it "multiplies a measure by a unitless measure" $ property $
       \(x, y, u)->
         Measure x u * Measure y Unitless == Measure (x*y) u
         && Measure x Unitless * Measure y u == Measure (x*y) u
+
     it "multiplies units to get compound units" $ property $
       \(Measure x u1, Measure y u2)->
         u1 /= Unitless && u2 /= Unitless ==>
           Measure x u1 * Measure y u2 == Measure (x * y) (u1 :*: u2)
+
+-- Spec support code
+
+instance Arbitrary Unit where
+  arbitrary = makeUnit
+    where
+      makeUnit :: Gen Unit
+      makeUnit = oneof [elements [Meter, Second, Watt, Unitless],
+                        liftM2 (:*:) makeUnit makeUnit]
+
+instance Arbitrary Measure where
+  arbitrary = Measure <$> arbitrary <*> arbitrary
+
