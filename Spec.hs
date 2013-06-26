bd35f005e546c62a9de53b10fdac5b4bdabc7088
Jonathan Perret <jonathan@ut7.fr>
2013-06-26 11:37:56 +0100

    better expressions
    Law of Continuity:
    	Experiments should be reproducible.  They should all fail the same way.

--- Spec.hs
+++ Spec.hs
@@ -3,15 +3,19 @@ import Test.Hspec
 import Test.HUnit
 import Test.QuickCheck
 import Data.Ratio
+import Control.Applicative
 
 data Unit = Meter | Second | Unitless | Unit :*: Unit
   deriving (Show, Eq)
 
-data Measure = Measure Rational Unit | InvalidMeasure
+data Measure = Measure { value :: Rational, unit :: Unit } | InvalidMeasure
   deriving (Show, Eq)
 
 instance Arbitrary Unit where
   arbitrary = elements [ Meter, Second, Unitless ]
+
+instance Arbitrary Measure where
+  arbitrary = Measure <$> arbitrary <*> arbitrary
 
 instance Num Measure where
   Measure x u1 + Measure y u2
@@ -20,8 +24,12 @@ instance Num Measure where
 
   negate (Measure x u) = Measure (-x) u
 
-  Measure x u * Measure y Unitless = Measure (x*y) u
-  Measure x Unitless * Measure y u = Measure (x*y) u
+  Measure x u1 * Measure y u2 =
+    Measure (x*y) (combine u1 u2)
+    where
+      combine Unitless u2 = u2
+      combine u1 Unitless = u1
+      combine u1 u2 = (u1 :*: u2)
 
 main = hspec $ do
   describe "Arithmetic on measures" $ do
@@ -29,8 +37,9 @@ main = hspec $ do
       \(x, y, u)->
         Measure x u + Measure y u == Measure (x + y) u
     it "does adds measures expressed in different units" $ property $
-      \(x, y, u, u2)->
-        u == u2 || Measure x u + Measure y u2 == InvalidMeasure
+      \(m1, m2)->
+        unit m1 /= unit m2 ==>
+          m1 + m2 == InvalidMeasure
     it "subtracts measures expressed in the same unit" $ property $
       \(x, y, u)->
         Measure x u - Measure y u == Measure (x - y) u
