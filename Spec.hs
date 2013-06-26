77e852475fa2e605544df4c17cf40f3380c58ab7
Jonathan Perret <jonathan@ut7.fr>
2013-06-26 10:35:13 +0100

    We can subtract
    Okay ... I'm going home to write the "I HATE RUBIK's CUBE HANDBOOK FOR
    DEAD CAT LOVERS" ...

--- Spec.hs
+++ Spec.hs
@@ -14,15 +14,19 @@ instance Arbitrary Unit where
   arbitrary = elements [ Meter, Second ]
 
 instance Num Measure where
-  (Measure x u1) + (Measure y u2)
-    | u1 == u2 = (Measure (x + y) u1)
+  Measure x u1 + Measure y u2
+    | u1 == u2 = Measure (x + y) u1
     | otherwise = InvalidMeasure
+  negate (Measure x u) = Measure (-x) u
 
 main = hspec $ do
   describe "Arithmetic on measures" $ do
     it "adds measures expressed in the same unit" $ property $
       \(x, y, u)->
-        (Measure x u) + (Measure y u) == (Measure (x + y) u)
+        Measure x u + Measure y u == Measure (x + y) u
     it "does adds measures expressed in different units" $ property $
       \(x, y, u, u2)->
-        u == u2 || (Measure x u) + (Measure y u2) == InvalidMeasure
+        u == u2 || Measure x u + Measure y u2 == InvalidMeasure
+    it "subtracts measures expressed in the same unit" $ property $
+      \(x, y, u)->
+        Measure x u - Measure y u == Measure (x - y) u
